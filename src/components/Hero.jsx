f10b7eccdf5308a64566a35443d04ed40676277f
eiwashita <erikoiwashita@gmail.com>
2025-08-31 19:52:06 -0600

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from './NavBar';
+import { letters } from '../data/index';
 
 const Hero = () => {
+  const [hoveredLetter, setHoveredLetter] = useState(null);
+
   return (
-    <div className="w-ful h-screen">
+    <div className="w-ful h-screen flex flex-col justify-center items-center">
       <NavBar />
+      <div className="flex flex-col md:items-center items-start xl:gap-y-10 gap-y-10 xl:mb-0 md:mb-20 mb-0">
+        <h1 className="flex flex-col xl:space-y-8 md:space-y-4 space-y-2 xl:text-6xl md:text-4xl text-3xl md:font-normal font-bolder text-yellow-500">
+          <span className="flex">
+            {letters.map((letter, index) => (
+              <span
+                key={index}
+                className="inline-block md:w-38 w-32 xl:-mr-20 -mr-24 relative"
+                onMouseEnter={() => setHoveredLetter(index)}
+                onMouseLeave={() => setHoveredLetter(null)}
+              >
+                {letter.char}
+                <img
+                  src={letter.img}
+                  alt={`Hover image ${index + 1}`}
+                  className={`xl:h-36 h-24 absolute bottom-full -translate-x-1/2 ${
+                    letter.rotate
+                  } ${hoveredLetter === index ? 'visible' : 'invisible'}`}
+                />
+              </span>
+            ))}
+          </span>
+        </h1>
+      </div>
     </div>
   );
 };
